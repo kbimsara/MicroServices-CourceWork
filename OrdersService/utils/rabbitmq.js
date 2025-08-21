b7db93b5aeea3b8c180a7e433c5ce62b30bd7ea2
Kavindu Bimsara <kavidubim2gamaethige@gmail.com>
2025-08-21 11:40:36 +0530

    update

--- OrdersService/utils/rabbitmq.js
+++ OrdersService/utils/rabbitmq.js
@@ -1,16 +1,18 @@
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 
 let channel;
 
 async function connectRabbitMQ() {
-    const connection = await amqp.connect(process.env.RABBITMQ_URL);
-    channel = await connection.createChannel();
+  const connection = await amqp.connect(process.env.RABBITMQ_URL);
+  channel = await connection.createChannel();
 }
 
 async function publish(queue, message) {
-    if (!channel) await connectRabbitMQ();
-    await channel.assertQueue(queue, { durable: true });
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true });
+  if (!channel) await connectRabbitMQ();
+  await channel.assertQueue(queue, { durable: true });
+  channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), {
+    persistent: true,
+  });
 }
 
 module.exports = { publish };
